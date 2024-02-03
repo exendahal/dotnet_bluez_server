5a260a3de956f62b6cb8b04384cada074a72079b
Santosh Dahal <exendahal@gmail.com>
2024-02-03 18:03:13 +0545

    feat: add support for .net 5,6 and 7

--- ble-server-linux/obj/Debug/net8.0/ble-server-linux.AssemblyInfo.cs
+++ ble-server-linux/obj/Debug/net8.0/ble-server-linux.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ble-server-linux")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0864cdf18905bff71c83565d29e88c588174e725")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc931675c4ea280a9ebf66829951207535992887")]
 [assembly: System.Reflection.AssemblyProductAttribute("ble-server-linux")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ble-server-linux")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
